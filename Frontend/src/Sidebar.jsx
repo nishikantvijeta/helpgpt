4ac2bb91ba58cfd2a90a198283d440e37ddfd44e
nishikantvijeta <128150871+nishikantvijeta@users.noreply.github.com>
2025-08-11 10:26:55 +0530

    Update Sidebar.jsx

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -1,117 +1,3 @@
-// import React from "react";
-// import "./Sidebar.css";
-// import { useContext, useEffect } from "react";
-// import { MyContext } from "./MyContext.jsx";
-// import { v1 as uuidv1 } from "uuid";
-
-// function Sidebar() {
-//     const {
-//         allThreads, setAllThreads, currThreadId, setNewChat, setPrompt,
-//         setReply, setCurrThreadId, setPrevChats, token
-//     } = useContext(MyContext);
-
-//     const getAllThreads = async () => {
-//         try {
-//             const response = await fetch("http://localhost:8080/api/thread", {
-//                 headers: {
-//                     "Content-Type": "application/json",
-//                     ...(token ? { "Authorization": `Bearer ${token}` } : {})
-//                 }
-//             });
-//             const res = await response.json();
-//             // Support both array and object response
-//             const threads = Array.isArray(res) ? res : res.threads || [];
-//             const filteredData = threads.map(thread => ({
-//                 threadId: thread.threadId,
-//                 title: thread.title
-//             }));
-//             setAllThreads(filteredData);
-//         } catch (err) {
-//             console.log(err);
-//         }
-//     };
-
-//     useEffect(() => {
-//         getAllThreads();
-//     }, [currThreadId]);
-
-//     const createNewChat = () => {
-//         setNewChat(true);
-//         setPrompt("");
-//         setReply(null);
-//         setCurrThreadId(uuidv1());
-//         setPrevChats([]);
-//     };
-
-//     const changeThread = async (newThreadId) => {
-//         setCurrThreadId(newThreadId);
-//         try {
-//             const response = await fetch(`http://localhost:8080/api/thread/${newThreadId}`, {
-//                 headers: {
-//                     "Content-Type": "application/json",
-//                     ...(token ? { "Authorization": `Bearer ${token}` } : {})
-//                 }
-//             });
-//             const res = await response.json();
-//             setPrevChats(res);
-//             setNewChat(false);
-//             setReply(null);
-//         } catch (err) {
-//             console.log(err);
-//         }
-//     };
-
-//     const deleteThread = async (threadId) => {
-//         try {
-//             const response = await fetch(`http://localhost:8080/api/thread/${threadId}`, {
-//                 method: "DELETE",
-//                 headers: {
-//                     "Content-Type": "application/json",
-//                     ...(token ? { "Authorization": `Bearer ${token}` } : {})
-//                 }
-//             });
-//             await response.json();
-//             setAllThreads(prev => prev.filter(thread => thread.threadId !== threadId));
-//             if (threadId === currThreadId) {
-//                 createNewChat();
-//             }
-//         } catch (err) {
-//             console.log(err);
-//         }
-//     };
-
-//     return (
-//         <section className="sidebar">
-//             <button onClick={createNewChat}>
-//                 <img src="src/assets/blacklogo.png" alt="gpt logo" className="logo" />
-//                 <span><i className="fa-solid fa-pen-to-square"></i></span>
-//             </button>
-//             <ul className="history">
-//                 {allThreads?.map((thread, idx) => (
-//                     <li
-//                         key={idx}
-//                         onClick={() => changeThread(thread.threadId)}
-//                         className={thread.threadId === currThreadId ? "highlighted" : ""}
-//                     >
-//                         {thread.title}
-//                         <i
-//                             className="fa-solid fa-trash"
-//                             onClick={e => {
-//                                 e.stopPropagation();
-//                                 deleteThread(thread.threadId);
-//                             }}
-//                         ></i>
-//                     </li>
-//                 ))}
-//             </ul>
-//             <div className="sign">
-//                 <p>Made by Nishikant &hearts;</p>
-//             </div>
-//         </section>
-//     )
-// }
-
-// export default Sidebar;
 import React from "react";
 import { useContext, useEffect } from "react";
 import { MyContext } from "./MyContext.jsx";
@@ -128,7 +14,7 @@ function Sidebar() {
             const response = await fetch("https://helpgpt-backened.onrender.com/api/thread", {
                 headers: {
                     "Content-Type": "application/json",
-                    ...(token ? { "Authorization": `Bearer ${token}` } : {})
+                    ...(token ? { "Authorization": Bearer ${token} } : {})
                 }
             });
             const res = await response.json();
@@ -161,7 +47,7 @@ function Sidebar() {
             const response = await fetch(`https://helpgpt-backened.onrender.com/api/thread/${newThreadId}`, {
                 headers: {
                     "Content-Type": "application/json",
-                    ...(token ? { "Authorization": `Bearer ${token}` } : {})
+                    ...(token ? { "Authorization": Bearer ${token} } : {})
                 }
             });
             const res = await response.json();
@@ -179,7 +65,7 @@ function Sidebar() {
                 method: "DELETE",
                 headers: {
                     "Content-Type": "application/json",
-                    ...(token ? { "Authorization": `Bearer ${token}` } : {})
+                    ...(token ? { "Authorization": Bearer ${token} } : {})
                 }
             });
             await response.json();
